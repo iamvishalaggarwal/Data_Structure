3aa3192d4454b6002317ddc4c0f6522a96853844
iamvishalaggarwal <vishalagarwal.872632@gmail.com>
2023-03-15 12:03:31 +0530

    Pattern 21

---
+++ Patterns/Pattern_21.java
@@ -0,0 +1,39 @@
+package Patterns;
+
+import java.util.Scanner;
+
+/*
+Pattern:
+****
+*  *
+*  *
+****
+*/
+public class Pattern_21 {
+    public static void pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i == 1 || i == n) {
+                for (int j = 1; j <= n; j++) {
+                    System.out.print("*");
+                }
+            } else {
+                for (int j = 1; j <= n; j++) {
+                    if (j == 1 || j == n)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter any number: ");
+        int number = scanner.nextInt();
+        pattern(number);
+        scanner.close();
+    }
+
+}
